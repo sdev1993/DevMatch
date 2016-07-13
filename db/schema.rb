2544eb364adfa38469b4fad79504ca567763eeaf
Steven Dornan <sdev1993@outlook.com>
2016-07-13 19:57:27 +0000

    Added associations for plans and users

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20160711185026) do
 
-  create_table "contacts", force: :cascade do |t|
+  create_table "contacts", force: :true do |t|
     t.string   "name"
     t.string   "email"
     t.text     "comments"
@@ -21,7 +21,14 @@ ActiveRecord::Schema.define(version: 20160711185026) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: :cascade do |t|
+    create_table "plans", force: :true do |t|
+    t.string   "name"
+    t.decimal   "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -34,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160711185026) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

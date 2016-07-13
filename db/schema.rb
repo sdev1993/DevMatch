5936f354136388d8544cd83d3dd6ede8d53a4cfb
Steven Dornan <sdev1993@outlook.com>
2016-07-13 20:08:06 +0000

    Updated schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711185026) do
+ActiveRecord::Schema.define(version: 20160713195137) do
 
-  create_table "contacts", force: :true do |t|
+  create_table "contacts", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.text     "comments"
@@ -21,14 +21,14 @@ ActiveRecord::Schema.define(version: 20160711185026) do
     t.datetime "updated_at"
   end
 
-    create_table "plans", force: :true do |t|
+  create_table "plans", force: :cascade do |t|
     t.string   "name"
-    t.decimal   "price"
+    t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: :true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20160711185026) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer "plan_id"
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

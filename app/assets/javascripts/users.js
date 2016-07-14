bc6dd066123652cb0652a985a93ab3adf6e14731
Steven Dornan <sdev1993@outlook.com>
2016-07-14 20:14:00 +0000

    message

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,42 +2,38 @@ $(document).ready(function() {
 
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
 
-  $('#form-submit-btn').click(function(event)) {
-
-   event.preventDefault();
-
-   $('input[type=submit]').prop('disabled', true);
-
+   $("#form-submit-btn").click(function(event) {
+    event.preventDefault();
+    $('input[type=submit]').prop('disabled', true);
     var error = false;
-    var ccNum = $('#card_number').val();
-    var cvcNum = $('#card_code').val();
-    var expMonth = $('#card_month').val();
-    var expYear = $('#card_year').val();
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
 
-  if(!error) {
+    if (!error) {
+      // Get the Stripe token:
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+    return false;
+  }); // form submission
 
-    Stripe.createToken ({
+  function stripeResponseHandler(status, response) {
+    // Get a reference to the form:
+    var f = $("#new_user");
 
-      number: ccNum,
-      cvc: cvcNum,
-      exp_month: expMonth,
-      exp_year: expYear
-       }, stripeResponseHandler);
+    // Get the token from the response:
+    var token = response.id;
+
+    // Add the token to the form:
+    f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+
+    // Submit the form:
+    f.get(0).submit();
   }
-
-      return false;
-                              });
-
-function stripeResponseHandler(status, response) {
-
-  var f = $('#new_user');
-
-  var token = response.id;
-
-  f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
-  
-  f.get(0).submit();
-
-}
-
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers:{registrations: 'users/registrations'}
+  devise_for :users, controllers:{Registrations: 'users/registrations'}
   resources :contacts
   get '/about' => 'pages#about'
   root 'pages#home'
